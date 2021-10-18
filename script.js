ed623dead28338c3c35a34ed046f48535d77b09a
Panos <adonel@gmail.com>
2021-10-18 20:57:01 +0000

    update

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 function compute()
 {
     var principal = document.getElementById("principal").value;
+    
+    //make sure that the principal amout is positive
     if (principal <= 0)
     {
         alert("Enter a positive amount");
-        document.getElementById("principal").focus();
+        setTimeout(function(){document.getElementById('principal').focus()}, 0);        
         return;
     }
+    
+    //Calculate the total final amount, based on input values
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
+
 
     document.getElementById("result").innerHTML = "<br>If you deposit <span style=\"background-color:yellow\">" + principal +"</span>," +
                                                   "<br>at an interest rate of <span style=\"background-color:yellow\">" + rate + "%</span>." +
@@ -18,6 +23,7 @@ function compute()
                                                   "<br>in the year <span style=\"background-color:yellow\">" + year + "</span>";
 }
 
+//This function is called when the user changes the interest rate input range
 function updateRate() 
 {
     var rateval = document.getElementById("rate").value;
